527effad40c45a481c090a15ad39f96a2c999f07
stephenkati <stephenkatuli88@gmail.com>
2023-02-08 13:54:20 +0300

    Fix linter errors

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ const closed = document.querySelector('#close');
 const navitem = document.querySelectorAll('.nav-item');
 
 hambuger.addEventListener('click', () => {
-    menu.classList.add('show-menu');
+  menu.classList.add('show-menu');
 });
 
 closed.addEventListener('click', () => {
-    menu.classList.remove('show-menu');
+  menu.classList.remove('show-menu');
 });
 
 navitem.forEach((n) => n.addEventListener('click', () => menu.classList.remove('show-menu')));
@@ -21,28 +21,25 @@ const morebtn = document.querySelector('.before-expand');
 const lessbtn = document.querySelector('.after-expand');
 
 more.addEventListener('click', (e) => {
-    e.preventDefault();
-    lessbtn.classList.toggle('after-expand');
+  e.preventDefault();
+  lessbtn.classList.toggle('after-expand');
 
-    if (morebtn.style.display === "none") {
-        morebtn.style.display = "block";
-    } 
-    else {
-        morebtn.style.display = "none";
-    }
-
+  if (morebtn.style.display === 'none') {
+    morebtn.style.display = 'block';
+  } else {
+    morebtn.style.display = 'none';
+  }
 });
 
 more.addEventListener('click', (e) => {
   e.preventDefault();
 
-    if (speakercardshide.style.display === "none") {
-      speakercardshide.style.display = "flex";
-    } 
-    else {
-      speakercardshide.style.display = "none";
-    }
-})
+  if (speakercardshide.style.display === 'none') {
+    speakercardshide.style.display = 'flex';
+  } else {
+    speakercardshide.style.display = 'none';
+  }
+});
 
 // array for featured speakers
 
@@ -53,7 +50,7 @@ const speakers = [
     speakerPhotoAlt: 'Peter Doig, contemporary artists',
     speakerTitle: 'Peter Doig',
     speakerLink: 'One of the most renowned living figurative painters.',
-    speakerDesc: 'The scenes he created while learning at Chelsea College of Art.'
+    speakerDesc: 'The scenes he created while learning at Chelsea College of Art.',
   },
   {
     id: '2',
@@ -61,7 +58,7 @@ const speakers = [
     speakerPhotoAlt: 'Jerry Saltz, an art critic.',
     speakerTitle: 'Jerry Saltz',
     speakerLink: 'An American art critic and columnist for New York magazine.',
-    speakerDesc: 'Jerry Saltz just won the esteemed award for his criticism at New York Magazine.'
+    speakerDesc: 'Jerry Saltz just won the esteemed award for his criticism at New York Magazine.',
   },
   {
     id: '3',
@@ -69,7 +66,7 @@ const speakers = [
     speakerPhotoAlt: 'Jacob Pinholster, technology expert in art',
     speakerTitle: 'Jacob Pinholster',
     speakerLink: 'Associate Professor in Arizona State University.',
-    speakerDesc: 'He teaches school of film. He also an expert in dance, art education, technology for performance and theatre.'
+    speakerDesc: 'He teaches school of film. He also an expert in dance, art education, technology for performance and theatre.',
   },
   {
     id: '4',
@@ -77,7 +74,7 @@ const speakers = [
     speakerPhotoAlt: 'Amy woodson culture expert and professor',
     speakerTitle: 'Amy Woodson',
     speakerLink: 'Professor at Loyola Marymount University in Los Angeles, California.',
-    speakerDesc: 'Her work concentrates on cultural reactions to industrialization, particularly the history of museums.'
+    speakerDesc: 'Her work concentrates on cultural reactions to industrialization, particularly the history of museums.',
   },
   {
     id: '5',
@@ -85,7 +82,7 @@ const speakers = [
     speakerPhotoAlt: 'Lydia Gatundu museum curator',
     speakerTitle: 'Lydia Gatundu Galavu',
     speakerLink: 'Curator at the National Museums of Kenya (NMK) Nairobi, Kenya.',
-    speakerDesc: 'She is an artist with a background in art education and exhibits design.'
+    speakerDesc: 'She is an artist with a background in art education and exhibits design.',
   },
   {
     id: '6',
@@ -93,15 +90,15 @@ const speakers = [
     speakerPhotoAlt: 'Wambui Collymore, art collector',
     speakerTitle: 'Wambui Collymore',
     speakerLink: 'She is an artist, consultant and gallery founder.',
-    speakerDesc: 'Wambui believes that anyone can buy art, not just high net-worth people.'
-  }
+    speakerDesc: 'Wambui believes that anyone can buy art, not just high net-worth people.',
+  },
 ];
 
 // display cards in website refactor
 const speakercardslayout = document.querySelector('.speakers-cards-layout');
 
 speakers.forEach((speaker) => {
-console.log(speaker.id);
+  // console.log(speaker.id);
 
   const speakercard = document.createElement('div');
   speakercard.classList.add('speaker-card');
@@ -122,5 +119,4 @@ console.log(speaker.id);
         `;
 
   speakercardslayout.appendChild(speakercard);
-
 });
